f20d3588aafe28fb895272ae49feca10cadb715b
Josh <lutz_143@yahoo.com>
2022-11-11 14:04:47 -0500

    created function for countdown by calculating number of questions and allocating 10 seconds for each

--- assets/js/start.js
+++ assets/js/start.js
@@ -7,19 +7,35 @@ var feedbackEl = document.querySelector("#feedback");
 var endScreenEl = document.querySelector("#end-screen");
 var questionsEl = document.querySelector("#questions");
 
-console.log(time);
+// console.log(time);
 console.log(startBtn);
 console.log(submitBtn);
 console.log(feedbackEl);
 console.log(endScreenEl);
 console.log(questionsEl);
 
-var timer;
+var timeLeft;
+numbQuestions = Object.keys(questions).length;
+var quizTime = numbQuestions * 10;
+
+console.log(numbQuestions);
+console.log(quizTime);
 
 // when user clicks start, a timer is initiated
 function startTimer(){
   startScreenEl.setAttribute("class", "hide");
   console.log('is working');
+
+  timeLeft = setInterval(countdown, 1000);
+}
+
+function countdown(){  
+  quizTime--;
+  time.textContent = quizTime;
+
+  if (quizTime <= 0){
+    window.location.reload();
+  }
 }
 
 startBtn.onclick = startTimer;
