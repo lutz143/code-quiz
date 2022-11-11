c7b5c15c4f1a1f757fee1208c520db77433a6185
Josh <lutz_143@yahoo.com>
2022-11-11 11:27:18 -0500

    added function to start timer and hide start screen

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,4 +1,5 @@
 // grab elements from DOM
+var startScreenEl = document.querySelector("#start-screen");
 var time = document.querySelector("#time");
 var startBtn = document.querySelector("#start");
 var submitBtn = document.querySelector("#submit");
@@ -6,11 +7,19 @@ var feedbackEl = document.querySelector("#feedback");
 var endScreenEl = document.querySelector("#end-screen");
 var questionsEl = document.querySelector("#questions");
 
-
-
 console.log(time);
 console.log(startBtn);
 console.log(submitBtn);
 console.log(feedbackEl);
 console.log(endScreenEl);
 console.log(questionsEl);
+
+var timer;
+
+// when user clicks start, a timer is initiated
+function startTimer(){
+  startScreenEl.setAttribute("class", "hide");
+  console.log('is working');
+}
+
+startBtn.onclick = startTimer;
