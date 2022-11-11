de037a9c129cbbc2fff2b2ccf2e30fcf6d7f58ab
Josh <lutz_143@yahoo.com>
2022-11-11 11:20:04 -0500

    added initial js start script for start of quiz and initial grab of DOM elements

---
+++ assets/js/start.js
@@ -0,0 +1,16 @@
+// grab elements from DOM
+var time = document.querySelector("#time");
+var startBtn = document.querySelector("#start");
+var submitBtn = document.querySelector("#submit");
+var feedbackEl = document.querySelector("#feedback");
+var endScreenEl = document.querySelector("#end-screen");
+var questionsEl = document.querySelector("#questions");
+
+
+
+console.log(time);
+console.log(startBtn);
+console.log(submitBtn);
+console.log(feedbackEl);
+console.log(endScreenEl);
+console.log(questionsEl);
