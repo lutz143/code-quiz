7e4fce42a4187817ad90681154b5bb3e156c3c4c
Josh <lutz_143@yahoo.com>
2022-11-11 14:52:30 -0500

    added function to randomly select question and iterate and show choices

--- assets/js/start.js
+++ assets/js/start.js
@@ -25,8 +25,12 @@ console.log(quizTime);
 function startTimer(){
   startScreenEl.setAttribute("class", "hide");
   console.log('is working');
+  
+  questionsEl.removeAttribute("class");
 
   timeLeft = setInterval(countdown, 1000);
+
+  retrieveQuestion();
 }
 
 function countdown(){  
@@ -34,7 +38,30 @@ function countdown(){
   time.textContent = quizTime;
 
   if (quizTime <= 0){
+    // call the end of the quiz, displaying score and asking for user's initials to log score
     window.location.reload();
+  }
+}
+
+function retrieveQuestion(){
+  quesTitle = questionsEl.querySelector("question-title");
+
+  var randIndex = Math.floor(Math.random() * questions.length);
+  var question = questions[randIndex];
+
+  console.log(question);
+
+  questionsEl.children[0].textContent = question.title;
+
+  for(var i=0; i < question.choices.length; i++) {
+    var choice = question.choices[i];
+    var choiceDecision = document.createElement("button");
+
+    choiceDecision.setAttribute('class', 'choice');
+    choiceDecision.setAttribute('value', choice);
+
+    choiceDecision.textContent = choice
+    questionsEl.children[1].appendChild(choiceDecision);
   }
 }
 
